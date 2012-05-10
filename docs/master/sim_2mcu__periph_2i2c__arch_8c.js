a8bc813b56d02fc3206ebef74ee207353e95baaf
Paparazzi Documentation buildbot <felix.ruess+docbot@gmail.com>
2012-05-10 04:01:41 +0000

    generated doxygen docs for master from cron job

--- docs/master/sim_2mcu__periph_2i2c__arch_8c.js
+++ docs/master/sim_2mcu__periph_2i2c__arch_8c.js
@@ -1,5 +1,7 @@
 var sim_2mcu__periph_2i2c__arch_8c =
 [
+    [ "i2c1_er_isr", "sim_2mcu__periph_2i2c__arch_8c.html#a33df19c3179deebb8a95f198327301d2", null ],
+    [ "i2c2_er_isr", "sim_2mcu__periph_2i2c__arch_8c.html#a3df7b2279162375f9355501159318219", null ],
     [ "i2c2_setbitrate", "sim_2mcu__periph_2i2c__arch_8c.html#abb48b4ab2fbd5d2de890575c279a8f60", null ],
     [ "i2c_event", "sim_2mcu__periph_2i2c__arch_8c.html#a05763355d0e087ba41feab61dd773fe6", null ],
     [ "i2c_hw_init", "sim_2mcu__periph_2i2c__arch_8c.html#a1cf8fc0d0bf6ed8866e3f0555d9bd68d", null ],
---
+++ docs/master/stm32_2subsystems_2imu_2imu__aspirin2__arch_8h.js
@@ -0,0 +1,5 @@
+var stm32_2subsystems_2imu_2imu__aspirin2__arch_8h =
+[
+    [ "imu_aspirin2_arch_int_disable", "stm32_2subsystems_2imu_2imu__aspirin2__arch_8h.html#a24545c6d740bf10eb0434e45f5812a94", null ],
+    [ "imu_aspirin2_arch_int_enable", "stm32_2subsystems_2imu_2imu__aspirin2__arch_8h.html#a18e42dd952bfa66a69bd96ef070d9531", null ]
+];
