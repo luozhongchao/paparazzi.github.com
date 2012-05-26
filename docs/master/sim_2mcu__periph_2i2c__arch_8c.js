5aa0cdc16d4381bc17bb75ee64bda7ba9846c1ae
Paparazzi Documentation buildbot <felix.ruess+docbot@gmail.com>
2012-05-26 04:03:25 +0000

    generated doxygen docs for master and v3.9 from cron job

--- docs/master/sim_2mcu__periph_2i2c__arch_8c.js
+++ docs/master/sim_2mcu__periph_2i2c__arch_8c.js
@@ -1,10 +1,7 @@
 var sim_2mcu__periph_2i2c__arch_8c =
 [
-    [ "i2c1_er_isr", "sim_2mcu__periph_2i2c__arch_8c.html#a33df19c3179deebb8a95f198327301d2", null ],
-    [ "i2c2_er_isr", "sim_2mcu__periph_2i2c__arch_8c.html#a3df7b2279162375f9355501159318219", null ],
     [ "i2c2_setbitrate", "sim_2mcu__periph_2i2c__arch_8c.html#abb48b4ab2fbd5d2de890575c279a8f60", null ],
     [ "i2c_event", "sim_2mcu__periph_2i2c__arch_8c.html#a05763355d0e087ba41feab61dd773fe6", null ],
-    [ "i2c_hw_init", "sim_2mcu__periph_2i2c__arch_8c.html#a1cf8fc0d0bf6ed8866e3f0555d9bd68d", null ],
     [ "i2c_idle", "sim_2mcu__periph_2i2c__arch_8c.html#a1b5ea0bb160946bc55b06ff3ca29503a", null ],
     [ "i2c_submit", "sim_2mcu__periph_2i2c__arch_8c.html#ad6ca066e9556deee2d78d493ea5cce30", null ]
 ];
