4f9e3cd4377dbd519244437c0bb21a1019a9eae7
Paparazzi Documentation buildbot <felix.ruess+docbot@gmail.com>
2012-11-20 04:02:50 +0000

    generated doxygen docs for master from cron job

--- docs/latest/stabilization__attitude__rc__setpoint_8h.js
+++ docs/latest/stabilization__attitude__rc__setpoint_8h.js
@@ -4,6 +4,7 @@ var stabilization__attitude__rc__setpoint_8h =
     [ "RC_UPDATE_FREQ", "stabilization__attitude__rc__setpoint_8h.html#a0dfaa7b497c5532a21ba0b8313b9311e", null ],
     [ "ROLL_DEADBAND_EXCEEDED", "stabilization__attitude__rc__setpoint_8h.html#ade5e3cdd965d34f8d9076b0724f54c02", null ],
     [ "YAW_DEADBAND_EXCEEDED", "stabilization__attitude__rc__setpoint_8h.html#a908110653d45fd82871ee768ffe38317", null ],
+    [ "stabilization_attitude_read_rc_roll_pitch_earth_quat", "stabilization__attitude__rc__setpoint_8h.html#ad2ad29601c78155e6cf592578253dafb", null ],
     [ "stabilization_attitude_read_rc_roll_pitch_quat", "stabilization__attitude__rc__setpoint_8h.html#a190bf96c3d055180f034b080fb11385b", null ],
     [ "stabilization_attitude_read_rc_setpoint_eulers", "stabilization__attitude__rc__setpoint_8h.html#a6ff670e2f11091810d8534deb2899185", null ]
 ];
