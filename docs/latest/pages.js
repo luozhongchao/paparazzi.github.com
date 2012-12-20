1c6a316dd75dc26696e6788768eb6fd1c75efc7b
Paparazzi Documentation buildbot <felix.ruess+docbot@gmail.com>
2012-12-20 17:27:18 +0000

    Generated doxygen docs for master from cron job

--- docs/latest/pages.js
+++ docs/latest/pages.js
@@ -1,5 +1,6 @@
 var pages =
 [
+    [ "Onboard Modules", "onboard_modules.html", "onboard_modules" ],
     [ "Paparazzi Technical Primers", "primer.html", null ],
     [ "Math Functions", "math.html", null ],
     [ "Todo List", "todo.html", null ]
